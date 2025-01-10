0dd8dde7a0a7d0fa82ac7af3b207b2ca1d2eb114
NAVEENKUMARJINKALA <jinkalanaveenkumar2024@gmail.com>
2025-01-10 12:44:56 +0530

    Delete 6.Python program to extract digits from tuple list.py

--- 6.Python program to extract digits from tuple list.py
+++
@@ -1,7 +0,0 @@
-Tupledigits=[(10,20,30),(10,20),(50,60,70,80,90),(58,68,78,98)]
-empty=[]
-for tup in Tupledigits:
-    for num in tup:
-        if isinstance(num,int):
-            empty.append(num)
-print(empty)
